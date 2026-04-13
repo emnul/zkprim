208bc87d68b859d1c529b35bd93c6914e7c17cab
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-04-13 16:23:22 -0400

    refactor: make Fp generic over a Modulus

--- src/field.rs
+++ src/field.rs
@@ -1,25 +1,58 @@
-use std::ops::{Add, Mul, Neg, Sub};
+use std::{
+    fmt::Debug,
+    marker::PhantomData,
+    ops::{Add, Mul, Neg, Sub},
+};
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Fp(pub u64);
+pub struct Fp<M: Modulus = MersennePrime>(pub u64, pub PhantomData<M>);
 
-impl Fp {
-    pub const PRIME_MODULUS: u64 = 2u64.pow(61) - 1;
+impl<M: Modulus> Fp<M> {
+    pub const PRIME_MODULUS: u64 = M::PRIME_MODULUS;
 
-    pub fn new(val: u64) -> Fp {
-        return Fp(val % Self::PRIME_MODULUS);
+    pub fn new(val: u64) -> Fp<M> {
+        return Fp(val % M::PRIME_MODULUS, PhantomData);
+    }
+
+    pub fn batch_multiply<F: FieldElement>(elements: &[F], scalar: &F) -> Vec<F> {
+        elements
+            .iter()
+            .map(|e| e.clone() * scalar.clone())
+            .collect()
     }
 }
 
-impl Add for Fp {
+impl<M: Modulus> Clone for Fp<M> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<M: Modulus> Copy for Fp<M> {}
+
+impl<M: Modulus> PartialEq for Fp<M> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<M: Modulus> Debug for Fp<M> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Fp")
+            .field(&self.0)
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+
+impl<M: Modulus> Add for Fp<M> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Self((self.0 + rhs.0) % Self::PRIME_MODULUS)
+        Self((self.0 + rhs.0) % M::PRIME_MODULUS, PhantomData)
     }
 }
 
-impl Neg for Fp {
+impl<M: Modulus> Neg for Fp<M> {
     type Output = Self;
 
     fn neg(self) -> Self::Output {
@@ -28,23 +61,23 @@ impl Neg for Fp {
             self
         // Find the additive inverse
         } else {
-            Self((Fp::PRIME_MODULUS - self.0) % Fp::PRIME_MODULUS)
+            Self((M::PRIME_MODULUS - self.0) % M::PRIME_MODULUS, PhantomData)
         }
     }
 }
 
-impl Mul for Fp {
+impl<M: Modulus> Mul for Fp<M> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
         // TODO: This naive implementation is inefficient, but it avoids an overflow problem
         // In the future, we can improve this using Montgomery multiplication
-        let value = (self.0 as u128 * rhs.0 as u128) % Fp::PRIME_MODULUS as u128;
-        Self(value as u64)
+        let value = (self.0 as u128 * rhs.0 as u128) % M::PRIME_MODULUS as u128;
+        Self(value as u64, PhantomData)
     }
 }
 
-impl Sub for Fp {
+impl<M: Modulus> Sub for Fp<M> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -59,7 +92,7 @@ pub trait FieldElement: Sized + Clone + Mul<Output = Self> {
     fn pow(&self, exp: u64) -> Self;
 }
 
-impl FieldElement for Fp {
+impl<M: Modulus> FieldElement for Fp<M> {
     // Use fast exponentiation algorithm https://math-sites.uncg.edu/sites/pauli/112/HTML/secfastexp.html
     fn pow(&self, mut exp: u64) -> Self {
         let mut res = Fp::one();
@@ -77,25 +110,35 @@ impl FieldElement for Fp {
     }
 
     fn one() -> Self {
-        Fp(1)
+        Fp(1, PhantomData)
     }
 
     fn zero() -> Self {
-        Fp(0)
+        Fp(0, PhantomData)
     }
 
     // Returns the multiplicative inverse of Fp via an extension of
     // Fermat's Little Theorem
     fn inv(&self) -> Option<Self> {
-        if *self == Fp::zero() {
+        if *self == Self::zero() {
             return None;
         }
 
         // a^p-1 = 1 mod p
         // a^p-1 * a^-1 = a^-1 mod p
         // a^p-2 = a^-1 mod p
-        Some(self.pow(Fp::PRIME_MODULUS - 2))
+        Some(self.pow(M::PRIME_MODULUS - 2))
     }
+}
+
+pub trait Modulus {
+    const PRIME_MODULUS: u64;
+}
+
+pub struct MersennePrime;
+
+impl Modulus for MersennePrime {
+    const PRIME_MODULUS: u64 = 2u64.pow(61) - 1;
 }
 
 #[cfg(test)]
@@ -104,62 +147,71 @@ mod tests {
 
     #[test]
     fn create_fp_within_range() {
-        let fp = Fp::new(0);
-        assert_eq!(fp, Fp(0));
+        let fp: Fp = Fp::new(0);
+        assert_eq!(fp, Fp::new(0));
 
-        let fp = Fp::new(Fp::PRIME_MODULUS - 1);
-        assert_eq!(fp, Fp(Fp::PRIME_MODULUS - 1));
+        let fp: Fp = Fp::new(MersennePrime::PRIME_MODULUS - 1);
+        assert_eq!(fp, Fp::new(MersennePrime::PRIME_MODULUS - 1));
 
-        let fp = Fp::new(49824);
-        assert_eq!(fp, Fp(49824));
+        let fp: Fp = Fp::new(49824);
+        assert_eq!(fp, Fp::new(49824));
     }
 
     #[test]
     fn create_fp_outside_range() {
-        let test_val = Fp::PRIME_MODULUS;
-        let fp = Fp::new(test_val);
-        assert_eq!(fp, Fp(0));
+        let test_val = MersennePrime::PRIME_MODULUS;
+        let fp: Fp = Fp::new(test_val);
+        assert_eq!(fp, Fp::new(0));
 
-        let test_val = Fp::PRIME_MODULUS + 42;
-        let fp = Fp::new(test_val);
-        assert_eq!(fp, Fp(test_val % Fp::PRIME_MODULUS));
+        let test_val = MersennePrime::PRIME_MODULUS + 42;
+        let fp: Fp = Fp::new(test_val);
+        assert_eq!(fp, Fp::new(test_val % MersennePrime::PRIME_MODULUS));
     }
 
     #[test]
     fn add_fp() {
         let test_val1 = 10121;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let test_val2 = 414;
         let fp2 = Fp::new(test_val2);
 
-        assert_eq!(fp1 + fp2, Fp((test_val1 + test_val2) % Fp::PRIME_MODULUS));
+        assert_eq!(
+            fp1 + fp2,
+            Fp::new((test_val1 + test_val2) % MersennePrime::PRIME_MODULUS)
+        );
 
-        let test_val1 = Fp::PRIME_MODULUS - 5;
-        let fp1 = Fp::new(test_val1);
-        let test_val2 = Fp::PRIME_MODULUS - 5;
+        let test_val1 = MersennePrime::PRIME_MODULUS - 5;
+        let fp1: Fp = Fp::new(test_val1);
+        let test_val2 = MersennePrime::PRIME_MODULUS - 5;
         let fp2 = Fp::new(test_val2);
 
-        assert_eq!(fp1 + fp2, Fp((test_val1 + test_val2) % Fp::PRIME_MODULUS));
+        assert_eq!(
+            fp1 + fp2,
+            Fp::new((test_val1 + test_val2) % MersennePrime::PRIME_MODULUS)
+        );
     }
 
     #[test]
     fn sub_fp() {
         let test_val1 = 10121;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let test_val2 = 414;
         let fp2 = Fp::new(test_val2);
 
-        assert_eq!(fp1 - fp2, Fp((test_val1 - test_val2) % Fp::PRIME_MODULUS));
+        assert_eq!(
+            fp1 - fp2,
+            Fp::new((test_val1 - test_val2) % MersennePrime::PRIME_MODULUS)
+        );
 
-        let test_val1 = Fp::PRIME_MODULUS - 5;
-        let fp1 = Fp::new(test_val1);
-        let test_val2 = Fp::PRIME_MODULUS - 5;
+        let test_val1 = MersennePrime::PRIME_MODULUS - 5;
+        let fp1: Fp = Fp::new(test_val1);
+        let test_val2 = MersennePrime::PRIME_MODULUS - 5;
         let fp2 = Fp::new(test_val2);
 
-        assert_eq!(fp1 - fp2, Fp(0));
+        assert_eq!(fp1 - fp2, Fp::new(0));
 
         let test_val1 = 10;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let test_val2 = 20;
         let fp2 = Fp::new(test_val2);
 
@@ -167,52 +219,58 @@ mod tests {
 
         assert_eq!(
             fp1 - fp2,
-            Fp::new(fp1.0 + (Fp::PRIME_MODULUS - fp2.0) % Fp::PRIME_MODULUS)
+            Fp::new(fp1.0 + (MersennePrime::PRIME_MODULUS - fp2.0) % MersennePrime::PRIME_MODULUS)
         );
     }
 
     #[test]
     fn neg_fp() {
-        let a = Fp::new(4);
-        assert_eq!(-a, Fp(Fp::PRIME_MODULUS - a.0));
+        let a: Fp = Fp::new(4);
+        assert_eq!(-a, Fp::new(MersennePrime::PRIME_MODULUS - a.0));
 
-        let a = Fp::new(0);
-        assert_eq!(-a, Fp(0));
+        let a: Fp = Fp::new(0);
+        assert_eq!(-a, Fp::new(0));
     }
 
     #[test]
     fn mul_fp() {
         let test_val1 = 10121;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let test_val2 = 414;
         let fp2 = Fp::new(test_val2);
 
         assert_eq!(
             fp1 * fp2,
-            Fp(((test_val1 as u128 * test_val2 as u128) % Fp::PRIME_MODULUS as u128) as u64)
+            Fp::new(
+                ((test_val1 as u128 * test_val2 as u128) % MersennePrime::PRIME_MODULUS as u128)
+                    as u64
+            )
         );
 
-        let test_val1 = Fp::PRIME_MODULUS - 5;
-        let fp1 = Fp::new(test_val1);
-        let test_val2 = Fp::PRIME_MODULUS - 5;
+        let test_val1 = MersennePrime::PRIME_MODULUS - 5;
+        let fp1: Fp = Fp::new(test_val1);
+        let test_val2 = MersennePrime::PRIME_MODULUS - 5;
         let fp2 = Fp::new(test_val2);
 
         assert_eq!(
             fp1 * fp2,
-            Fp(((test_val1 as u128 * test_val2 as u128) % Fp::PRIME_MODULUS as u128) as u64)
+            Fp::new(
+                ((test_val1 as u128 * test_val2 as u128) % MersennePrime::PRIME_MODULUS as u128)
+                    as u64
+            )
         );
     }
 
     #[test]
     fn pow_fp() {
         let test_val1 = 10;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let exp = 10;
 
         assert_eq!(fp1.pow(exp), Fp::new(test_val1.pow(exp as u32)));
 
         let test_val1 = 2;
-        let fp1 = Fp::new(test_val1);
+        let fp1: Fp = Fp::new(test_val1);
         let exp = 61;
 
         assert_eq!(fp1.pow(exp), Fp::new(test_val1.pow(exp as u32)));
@@ -220,8 +278,8 @@ mod tests {
 
         // Use Fermat's Little Theorem to test pow
         let test_val1 = 3234; // a can be any number in the group
-        let exp = Fp::PRIME_MODULUS - 1; // Group order is p - 1
-        let fp1 = Fp::new(test_val1);
+        let exp = MersennePrime::PRIME_MODULUS - 1; // Group order is p - 1
+        let fp1: Fp = Fp::new(test_val1);
 
         assert_eq!(fp1.pow(exp), Fp::one());
     }
@@ -229,12 +287,17 @@ mod tests {
     #[test]
     fn inv_fp() {
         // 1/0 is undefined
-        assert_eq!(Fp::zero().inv(), None);
+        assert_eq!(Fp::<MersennePrime>::zero().inv(), None);
 
         // inverse of the identity element is itself
-        assert_eq!(Fp::one().inv(), Some(Fp::one()));
+        assert_eq!(Fp::<MersennePrime>::one().inv(), Some(Fp::one()));
 
         // Use Fermant's Little Theorem to test inv
-        assert_eq!(Fp(10).pow(Fp::PRIME_MODULUS - 1).inv(), Some(Fp::one()));
+        assert_eq!(
+            Fp::<MersennePrime>::new(10)
+                .pow(MersennePrime::PRIME_MODULUS - 1)
+                .inv(),
+            Some(Fp::one())
+        );
     }
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,6 +2,6 @@ use zkprim::field::Fp;
 
 #[test]
 fn field_element_is_accessible() {
-    let a = Fp::new(7);
+    let a: Fp = Fp::new(7);
     assert_eq!(a.0, 7);
 }
